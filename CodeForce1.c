122fce7d87ea83706c2d44501647e00851ad1bc7
ferdouszihad <f.zihad333@gmail.com>
2023-09-05 11:42:49 +0600

    condition problem solved

---
+++ CodeForce1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n1,n2;
+    scanf("%d%d",&n1,&n2);
+
+    if(n1>=n2){
+        printf("Yes\n");
+    }else{
+        printf("No\n");
+    }
+
+
+
+
+    return 0;
+}
