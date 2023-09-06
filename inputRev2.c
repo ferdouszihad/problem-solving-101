bd20bb8904ee4b7fa47b76e7435f28e9f594b660
ferdouszihad <f.zihad333@gmail.com>
2023-09-06 16:18:03 +0600

    digit problem is solved

---
+++ inputRev2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+    int n,i ,num,count =0,a[10],j;
+    scanf("%d",&n);
+    char arr[n];
+    for(i=0;i<n;i++){
+        
+        scanf("%d",&arr[i]);   
+    }
+
+
+    for(i=0;i<n;i++){
+        
+        num = arr[i];
+        if(num == 0){
+            printf("0\n");
+            continue;
+        }
+
+       while(num>0){
+        printf("%d ",num%10);
+        num/=10;
+
+       }
+        printf("\n");
+       
+    }
+
+
+    return 0;
+}
